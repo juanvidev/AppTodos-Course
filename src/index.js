afbafafc40826f04a1a52f4a73313bcef39f6edc
juanvidev <juanvillota33@gmail.com>
2022-02-16 00:23:33 -0500

    Borrar console.log

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { crearTodoHtml } from './js/componentes';
 export const todoList = new TodoList();
 
 todoList.todos.forEach(crearTodoHtml);
-console.log(todoList.todos)
 
 // todoList.todos.forEach(todo => {
 //     crearTodoHtml(todo);
